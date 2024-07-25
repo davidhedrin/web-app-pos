b94bc906db0c27ff6a91a7b307dc642bf7a1dcba
davidhedrin <davidhedrin123@gmail.com>
2024-07-25 08:29:56 +0700

    Uncomment field point in add member sales page

--- src/components/pages/Sales.vue
+++ src/components/pages/Sales.vue
@@ -3045,10 +3045,10 @@
                       <label class="form-label mb-0" for="tanggal_lahir">Tanggal Lahir</label>
                       <input v-model="dataInputMember.tanggal_lahir" class="form-control bg-transparent" id="tanggal_lahir" type="date">
                     </div>
-                    <!-- <div class="mb-1">
+                    <div class="mb-1">
                       <label class="form-label mb-0" for="point_member">Point member</label>
                       <input v-model="dataInputMember.point" @input="dataInputMember.point = $root.inputTextFormatPrice($event);" class="form-control bg-transparent" id="point_member" type="text" placeholder="Masukkan point member">
-                    </div> -->
+                    </div>
                     <div class="pt-2 mb-3 text-end">
                       <button class="btn btn-primary btn-sm" type="submit">Submit</button>
                     </div>
